ab0f3a2f8cbab12fb11a4b6b88680d1c42d02ded
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-03-30 14:52:11 -0700

    Create navbar html
    Need to figure out redirecting links so they apply to buttons (something
    about states as opposed to different pages)

--- open-classroom/angular-src/src/app/components/navbar/navbar.component.ts
+++ open-classroom/angular-src/src/app/components/navbar/navbar.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
