1517953a750e088ac0fc1b08258921a771fb3ed2
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-05-07 12:09:14 -0700

    Catch up development branch.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Database name and location
-    // database: 'mongodb://localhost:27017/open-classroom', // For Testing
-    database: 'mongodb://open-classroom:0pen-classroom@ds129281.mlab.com:29281/open-classroom',
+    database: 'mongodb://localhost:27017/open-classroom', // For Testing
+    //database: 'mongodb://open-classroom:0pen-classroom@ds129281.mlab.com:29281/open-classroom',
     secret: 'password'
 }
