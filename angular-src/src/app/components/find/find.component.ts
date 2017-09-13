eacf0fc3e66574b603ce5a185b5175821ad2e784
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-09-13 00:02:30 -0700

    Console Log the accessing of the JSON.

--- angular-src/src/app/components/find/find.component.ts
+++ angular-src/src/app/components/find/find.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 export class FindComponent implements OnInit {
   name : String;
   day : String;
+  time : string;
   buildingList : Object;
 
   constructor(
@@ -22,6 +23,25 @@ export class FindComponent implements OnInit {
 
   onSubmit() {
     this.buildingService.getBuildings(this.name, this.day).subscribe(buildingList => {
+      // Where we do the data processing
+      let openBuildingJSON = buildingList['OpenBuilding'];
+      for (var build in openBuildingJSON)
+      {
+        let openRoomJSON = openBuildingJSON[build]['rooms'];
+        for (var room in openRoomJSON)
+        {
+          let openClassJSON = openBuildingJSON[build]['rooms'][room]['class'];
+          for (var oclass in openClassJSON)
+          {
+            let timesJSON = openClassJSON[oclass];
+            if (parseInt(timesJSON['st'], 10) >= parseInt(this.time, 10))
+            {
+              console.log(timesJSON['st']);
+            }
+          }
+        }
+      }
+      
       this.buildingList = buildingList.OpenBuilding;
     },
     err => {
