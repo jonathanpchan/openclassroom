6f5be2ac4f6f6c963173967ae3bfdf3c69356f2a
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-09-14 22:55:16 -0700

    Implement By Building and added some CSS styling

--- angular-src/src/app/components/find/find.component.ts
+++ angular-src/src/app/components/find/find.component.ts
@@ -11,8 +11,7 @@ import {Router} from '@angular/router';
 export class FindComponent implements OnInit {
   name : string;
   day : string;
-  time : string;
-  buildingList : Object;
+  roomsList = [];
 
   constructor(
     private buildingService:BuildingsService,
@@ -23,68 +22,125 @@ export class FindComponent implements OnInit {
 
   onSubmit() {
     this.buildingService.getBuildings(this.name, this.day).subscribe(buildingList => {
-      // Where we do the data processing
-      let openBuildingJSON = buildingList['OpenBuilding'];//list of buildings
+      this.roomsList = [];
+      let openBuildingJSON = buildingList['OpenBuilding']; // list of buildings
       for (var build in openBuildingJSON)
       {
-        console.log(openBuildingJSON[build].name);
         let openRoomJSON = openBuildingJSON[build]['rooms'];
-        for (var room in openRoomJSON)//list of rooms in that given building
+
+        for (var room in openRoomJSON) //list of rooms in that given building
         {
-          console.log(openBuildingJSON[build].name + "-" + openRoomJSON[room].name)//log name
+          var arr = new Array(24*12);
           let openClassJSON = openRoomJSON[room]['class']; //list of classes in that building
           for (var oclass in openClassJSON)
           {
-            //let timesJSON = openClassJSON[oclass];
-            var st = openClassJSON[oclass].st;
-            var et = openClassJSON[oclass].et
-            var start = this.timeFormat(st);
-            var end = this.timeFormat(et);
-            console.log(start + " - " + end);
+            let timesJSON = openClassJSON[oclass];
+            var st = timesJSON.st;
+            var et = timesJSON.et;
 
-            // if (parseInt(timesJSON['st'], 10) >= parseInt(this.time, 10))
-            // {
-            //   console.log(timesJSON['st']);
-            // }
+            // Needs to be reviewed. This is static but should be dynamic.
+            for (var i = 0; i < 24*12; i++)
+            {
+              if (timesJSON.st == i*5)
+              {
+                if (timesJSON.et == (i+1)*5)
+                {
+                  arr[i] = arr[i] | 1;
+                }
+                else if (timesJSON.et < (i+1)*5)
+                {
+                  arr[i] = arr[i] | 1;
+                }
+                else
+                {
+                  arr[i] = arr[i] | 1;
+                }
+              }
+              else if (timesJSON.st < i*5)
+              {
+                if (timesJSON.et == (i+1)*5)
+                {
+                  arr[i] = arr[i] | 1;
+                }
+                else if (timesJSON.et < (i+1)*5)
+                {
+                  if (timesJSON.et >= i*5)
+                  {
+                    //arr[i] = ((i+1)*60 - timesJSON.et)/60;
+                    arr[i] = arr[i] | 1;
+                  }
+                  else
+                  {
+                    arr[i] = arr[i] | 0;
+                  }
+                }
+                else 
+                {
+                  arr[i] = arr[i] | 1;
+                }
+              }
+              else
+              {
+                arr[i] = arr[i] | 0;
+              }
+            }
           }
+          //console.log(openRoomJSON[room].name+" "+arr);
+          this.roomsList.push({ name : openRoomJSON[room].name, room : arr});
         }
       }
-
-      this.buildingList = buildingList.OpenBuilding;
+      document.getElementById("input").style.display = "none";
+      document.getElementById("table").style.display = "block";
+      document.getElementById("back").style.display = "block";
     },
     err => {
       console.log(err);
     });
   }
 
-  timeFormat(time)
-  {
-    var t;
-    var minutes = time;
+  // timeFormat(time)
+  // {
+  //   var t;
+  //   var minutes = time;
 
-    if(minutes>=780)    {
-      minutes-=720;//if its 13 o'clock you take off 12 hours or 720 mins
-    }
-    // TODO: remove this for deployment as it's unneeded
-    else if(minutes < 60)    {
-      minutes+=720;//adding 12 hours if its before 1 AM
-    }
+  //   if(minutes>=780)    {
+  //     minutes-=720;//if its 13 o'clock you take off 12 hours or 720 mins
+  //   }
+  //   // TODO: remove this for deployment as it's unneeded
+  //   else if(minutes < 60)    {
+  //     minutes+=720;//adding 12 hours if its before 1 AM
+  //   }
 
-    t = (minutes - minutes%60)/60 + ":";//calculating hours
+  //   t = (minutes - minutes%60)/60 + ":";//calculating hours
 
-    if(minutes%60==0)    {//formating minutes toFixed and to Prevision dont work
-      t += "00";
-    }else    {
-      t += time%60;
-    }
+  //   if(minutes%60==0)    {//formating minutes toFixed and to Prevision dont work
+  //     t += "00";
+  //   }else    {
+  //     t += time%60;
+  //   }
 
-    if(time>720)    {//setting AM/PM based on the original time
-      t+= " PM";
-    }
-    else    {
-      t+= " AM";
-    }
+  //   if(time>720)    {//setting AM/PM based on the original time
+  //     t+= " PM";
+  //   }
+  //   else    {
+  //     t+= " AM";
+  //   }
 
-    return t;
+  //   return t;
+  // }
+
+  onBack() {
+    document.getElementById("input").style.display = "block";
+    document.getElementById("table").style.display = "none";
+    document.getElementById("back").style.display = "none";
   }
+
+  getClass(value : any) : string {
+    return 'opentime';
+  }
+}
+
+class openRooms {
+  name : string;
+  openclass : [number];
 }
