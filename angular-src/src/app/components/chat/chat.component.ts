31afdb4d2bf7364bcd9c9f7c37603f66d75d465a
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-11-15 11:03:51 -0800

    Update App.js for live deployment (implementing chat)

--- angular-src/src/app/components/chat/chat.component.ts
+++ angular-src/src/app/components/chat/chat.component.ts
@@ -72,6 +72,20 @@ export class ChatComponent implements OnInit {
     this.message='';
   }
 
+  // TODO: USED WHILE WE DO NOT HAVE THE REST OF THE FUNCTIONALITY OF OUR CODE
+  stub() {
+    // Make sure there is a connection, there is a message, and the message is not just white space
+    if (this.connection == null || this.sendee == null || this.message == null || this.message.trim().length == 0) {
+      this.flashMessage.show('Cannot send message. Did you join a room?', {cssClass: 'alert-danger', timeout: 3000});
+    }
+    else {
+      // Send message to room with message
+      this.chatService.sendMessage(this.currentRoom, this.sendee, this.message).subscribe();
+    }
+    // Clear message
+    this.message='';
+  }
+
   back() {
     this.sendee = null;
     this.showBack = false;
